fd89adf630859201a71599996b234ddeb39f4aed
UI Manufaktur <ui.manufaktur@gmail.com>
2017-11-29 10:23:01 +0100

    Update package.d

--- uim/imdb/package.d
+++ uim/imdb/package.d
@@ -8,4 +8,15 @@ enum Relation : string {
 	OneToMany = "1-M",
 	ManyToOne = "M-1",
 	ManyToMany = "M-M"
-  }
+}
+Relation toRelation(string nameOfRelation) {
+	switch (nameOfRelation) {
+		case "1": return Relation.EmbeddedOne;
+		case "M": return Relation.EmbeddedMany;
+		case "1-1": return Relation.OneToOne;
+		case "1-M": return Relation.OneToMany;
+		case "M-1": return Relation.ManyToOne;
+		case "M-M": return Relation.ManyToMany;
+	default: return Relation.None;
+	}
+}
